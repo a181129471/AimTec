cc7a6b922e564d052ea5065c2b3c145d1c3d37af
Andy <rahmanjan1989630@gmail.com>
2017-09-19 01:45:46 +0400

    Add files via upload

---
+++ Auto Leveler/C/MenuManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aimtec;
+using Aimtec.SDK.Menu;
+using Aimtec.SDK.Menu.Components;
+using Aimtec.SDK.Orbwalking;
+using Aimtec.SDK.Util;
+
+namespace Auto_Leveler {
+
+    class MenuManager {
+
+        private static readonly Menu Menu = new Menu("autoLeveler", "Auto Leveler", true);
+
+        private static string _championName;
+
+        private static bool _validConfiguration;
+
+        public static Dictionary<SpellSlot, int> LevelPriorities = new Dictionary<SpellSlot, int>();
+
+        public static Dictionary<SpellSlot, int> LevelAts = new Dictionary<SpellSlot, int>();
+
+        public static void Create() {
+            _championName = ObjectManager.GetLocalPlayer().ChampionName;
+
+            Menu autoLevelerMenu = new Menu(_championName, _championName) {
+                new Menu("levels", "Levels") {
+                    new MenuSlider("q", "Q", 1, 1, 4),
+                    new MenuSlider("w", "W", 2, 1, 4),
+                    new MenuSlider("e", "E", 3, 1, 4),
+                    new MenuSlider("r", "R", 4, 1, 4)
+                },
+                new Menu("levelAt", "Level at") {
+                    new MenuSlider("q", "Q", 2, 2, 18),
+                    new MenuSlider("w", "W", 2, 2, 18),
+                    new MenuSlider("e", "E", 2, 2, 18),
+                    new MenuSlider("r", "R", 2, 2, 18)
+                },
+                new MenuBool("atleastOnce", "Level QWE atleast once"),
+                new MenuSlider("startAt", "Start the Auto leveler at level", 2, 2, 18)
+            };
+            MenuBool humanizer = new MenuBool("humanizer", "Humanizer Delay");
+
+            autoLevelerMenu["levels"].OnValueChanged += LevelsOnOnValueChanged;
+            autoLevelerMenu["levelAt"].OnValueChanged += LevelAtOnOnValueChanged;
+
+            Menu.Add(autoLevelerMenu);
+            Menu.Add(humanizer);
+
+            Menu.Attach();
+
+            ReadLevelAts();
+
+            if (HasValidPriorities()) {
+                ReadSpellValues();
+                _validConfiguration = true;
+            }
+        }
+
+        public static int StartAt() {
+            return Menu[_championName]["startAt"].Value;
+        }
+
+        public static bool LevelQWEAtleastOnce() {
+            return Menu[_championName]["atleastOnce"].Enabled;
+        }
+
+        public static bool HumanizerEnabled() {
+            return Menu["humanizer"].Enabled;
+        }
+
+        public static bool HasValidConfiguration() {
+            return _validConfiguration;
+        }
+
+        private static void ReadSpellValues() {
+            LevelPriorities[SpellSlot.Q] = Menu[_championName]["levels"]["q"].Value;
+            LevelPriorities[SpellSlot.W] = Menu[_championName]["levels"]["w"].Value;
+            LevelPriorities[SpellSlot.E] = Menu[_championName]["levels"]["e"].Value;
+            LevelPriorities[SpellSlot.R] = Menu[_championName]["levels"]["r"].Value;
+        }
+
+        private static void LevelAtOnOnValueChanged(MenuComponent sender, ValueChangedArgs args) {
+            LevelAts[SpellSlot.Q] = Menu[_championName]["levelAt"]["q"].Value;
+            LevelAts[SpellSlot.W] = Menu[_championName]["levelAt"]["w"].Value;
+            LevelAts[SpellSlot.E] = Menu[_championName]["levelAt"]["e"].Value;
+            LevelAts[SpellSlot.R] = Menu[_championName]["levelAt"]["r"].Value;
+        }
+
+        private static void ReadLevelAts() {
+            LevelAts[SpellSlot.Q] = Menu[_championName]["levelAt"]["q"].Value;
+            LevelAts[SpellSlot.W] = Menu[_championName]["levelAt"]["w"].Value;
+            LevelAts[SpellSlot.E] = Menu[_championName]["levelAt"]["e"].Value;
+            LevelAts[SpellSlot.R] = Menu[_championName]["levelAt"]["r"].Value;
+        }
+
+        private static void LevelsOnOnValueChanged(MenuComponent sender, ValueChangedArgs args) {
+            if (HasValidPriorities()) {
+                _validConfiguration = true;
+                ReadSpellValues();
+            }
+            else {
+                _validConfiguration = false;
+            }
+        }
+
+        private static bool HasValidPriorities() {
+            List<int> priorities = new List<int>(new int[] {
+                Menu[_championName]["levels"]["q"].Value, Menu[_championName]["levels"]["w"].Value,
+                Menu[_championName]["levels"]["e"].Value, Menu[_championName]["levels"]["r"].Value
+            });
+
+            foreach (int priority in priorities) {
+                if (priorities.FindAll(x => x == priority).Count > 1) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+
+}
---
+++ Auto Leveler/C/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Aimtec;
+using Aimtec.SDK.Events;
+using Aimtec.SDK.Util;
+
+namespace Auto_Leveler {
+
+    public class Program {
+
+        private static readonly int BaseY = (int) (Render.Height - Render.Height / 4);
+        private static readonly int BaseX = (int) (Render.Width / 2.0);
+
+        private static readonly string InvalidSpellConfigurationText = "Invalid spell priority configuration";
+
+        private static readonly Random Random = new Random();
+
+        private static bool _needToLevelCurrent;
+        private static int _currentOldlevel;
+        private static SpellSlot _currentSpell;
+
+        public static void Main(string[] args) {
+            GameEvents.GameStart += GameEvents_GameStart;
+        }
+
+        private static void GameEvents_GameStart() {
+            MenuManager.Create();
+
+            Game.OnUpdate += Game_OnUpdate;
+            Obj_AI_Base.OnLevelUp += ObjAiBaseOnOnLevelUp;
+        }
+
+        private static void Game_OnUpdate() {
+            if (_needToLevelCurrent) {
+                ObjectManager.GetLocalPlayer().SpellBook.LevelSpell(_currentSpell);
+
+                if (ObjectManager.GetLocalPlayer().SpellBook.GetSpell(_currentSpell).Level != _currentOldlevel) {
+                    _needToLevelCurrent = false;
+                }
+            }
+        }
+
+
+        private static void ObjAiBaseOnOnLevelUp(Obj_AI_Base objAi, Obj_AI_BaseLevelUpEventArgs e) {
+            if (e.NewLevel < MenuManager.StartAt()) {
+                return;
+            }
+
+            if (objAi.GetType() != typeof(Obj_AI_Hero)) {
+                return;
+            }
+
+            Obj_AI_Hero hero = (Obj_AI_Hero) objAi;
+            if (hero.Name == null || !hero.Name.Equals(ObjectManager.GetLocalPlayer().Name)) {
+                return;
+            }
+
+            List<KeyValuePair<SpellSlot, int>> pairs = MenuManager.LevelPriorities.ToList();
+            pairs.Sort((p1, p2) => p1.Value.CompareTo(p2.Value));
+
+            foreach (KeyValuePair<SpellSlot, int> pair in pairs) {
+                Spell spell = hero.SpellBook.GetSpell(pair.Key);
+
+                if (MenuManager.LevelQWEAtleastOnce() && e.NewLevel <= 3) {
+                    if (spell.Level == 0 && LevelSpell(pair.Key, e.NewLevel)) {
+                        return;
+                    }
+                }
+                else {
+                    if (e.NewLevel >= MenuManager.LevelAts[pair.Key]) {
+                        if (LevelSpell(pair.Key, e.NewLevel)) {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool CanLevelUpSpell(SpellSlot spell, int combatLevel) {
+            Obj_AI_Hero player = ObjectManager.GetLocalPlayer();
+            int spellLevel = player.SpellBook.GetSpell(spell).Level;
+            string champion = player.ChampionName;
+
+            if (spell.Equals(SpellSlot.R) && !champion.Equals("Udyr")) {
+                // ulti level 1
+                if (combatLevel >= 6 && combatLevel < 11 && spellLevel == 0) {
+                    return true;
+                }
+
+                // ulti level 2
+                if (combatLevel >= 11 && combatLevel < 16 && spellLevel <= 1) {
+                    return true;
+                }
+
+                // ulti level 3
+                if (combatLevel >= 16 && spellLevel <= 2) {
+                    // ryze r is max 2
+                    if (champion.Equals("Ryze") && spellLevel == 2) {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            else {
+                // level 1-2, max level is 1
+                if (combatLevel >= 1 && combatLevel < 3 && spellLevel == 0) {
+                    return true;
+                }
+
+                // level 3-4, max level is 2
+                if (combatLevel >= 3 && combatLevel < 5 && spellLevel <= 1) {
+                    return true;
+                }
+
+                // level 5-6, max level is 3
+                if (combatLevel >= 5 && combatLevel < 7 && spellLevel <= 2) {
+                    return true;
+                }
+
+                // level 7-8, max level is 4
+                if (combatLevel >= 7 && combatLevel < 9 && spellLevel <= 3) {
+                    return true;
+                }
+
+                // level >= 9, max level is 5
+                if (combatLevel >= 9 && spellLevel <= 4) {
+                    return true;
+                }
+
+                // level >= 11 for ryze, max level for q is 6
+                if (champion.Equals("Ryze") && spell.Equals(SpellSlot.Q) && combatLevel >= 11 && spellLevel <= 5) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool LevelSpell(SpellSlot spellSlot, int combatLevel) {
+            if (CanLevelUpSpell(spellSlot, combatLevel)) {
+                _needToLevelCurrent = true;
+                _currentSpell = spellSlot;
+                _currentOldlevel = ObjectManager.GetLocalPlayer().SpellBook.GetSpell(spellSlot).Level;
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+}
